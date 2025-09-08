193bf0952a2578d704f42505ecdb4be22dac4d37
mario4off <mario31991@gmail.com>
2025-09-08 19:28:05 +0200

    errores implementados + 404 + validación

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -7,7 +6,6 @@ import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
-  CardsSkeleton,
 } from "@/app/ui/skeletons";
 
 export default async function Page() {
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,7 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
-import {montserrat} from "@/app/ui/fonts";
+import SideNav from "@/app/ui/dashboard/sidenav";
 
-export default function Layout ({children}: {children :React.ReactNode}){
-    return (
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
